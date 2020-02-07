490c2257165703e22c6c353afd700663bb04ae36
Joshua Bowers <joshman160@hotmail.com>
2020-02-07 15:56:16 -0500

    Update badntp.py

--- opt/ntp/badntp.py
+++ opt/ntp/badntp.py
@@ -4,11 +4,12 @@
 import os
 import re
 import pathlib
+
 #Define variables
-ntpserver1="ntp server x.x.x.x prefer"
-ntpserver2="ntp server x.x.x.x"
-ntpserver3="ntp server vrf Mgmt-intf x.x.x.x prefer"
-ntpserver4="ntp server vrf Mgmt-intf x.x.x.x"
+ntpserver1="ntp server 1.1.1.1 prefer"
+ntpserver2="ntp server 2.2.2.2"
+ntpserver3="ntp server vrf Mgmt-intf 1.1.1.1 prefer"
+ntpserver4="ntp server vrf Mgmt-intf 2.2.2.2"
 ntpservice1="service timestamps debug datetime msec localtime show-timezone"
 ntpservice2="service timestamps log datetime msec localtime show-timezone"
 timezone1="clock timezone EST -5"
@@ -17,14 +18,15 @@ daylight1="clock summer-time EDT recurring"
 daylight2="clock summer-time EDT 2 Sun Mar 2:00 1 Sun Nov 2:00 60"
 ntpsource="ntp source"
 ntpperiod="ntp clock-period"
+
 #Start Examining configs for Missing NTP configurations
 header = "The following have bad NTP configurations"
 print(header)
 dir = "/opt/modconfigs"
 for host in os.listdir(dir):
     config_path = os.path.join(dir, host)
-# If statment will ignore  Core Switches since these are NTP servers
-    if host.endswith("switch1") or host.endswith("switch2"):
+# If statment will ignore Core Switches since these are NTP servers
+    if host.endswith("coreswith1") or host.endswith("coreswith2"):
       p=0
     else:
 #NTP Server
@@ -78,7 +80,7 @@ print(header)
 dir = "/opt/modconfigs"
 for host in os.listdir(dir):
     # If statment will ignore Core Switches since these are NTP servers
-    if host.endswith("switch1") or host.endswith("switch2"):
+    if host.endswith("coreswith1") or host.endswith("coreswith2"):
       p=0
     else:
       config_path = os.path.join(dir, host)
@@ -88,6 +90,7 @@ for host in os.listdir(dir):
              #Ignore NTP Server
              p=1
           if line.startswith(ntpsource) or line.startswith(ntpperiod):
+            #Another Ignore
              p=1
           elif line.startswith("ntp") and p is 0:
              print(host,"has extra NTP command", line)
